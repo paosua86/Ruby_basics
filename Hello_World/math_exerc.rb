33239ea34f81d974167f48e0e57df51e66af6684
paosua86 <paolasuarezb1986@gmail.com>
2022-09-28 18:21:12 -0500

    add files

---
+++ Hello_World/math_exerc.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+numberOne = 6
+numberTwo = 8
+numberThree = 5
+numberFour = 12
+numberFive = 36
+
+testOne = numberTwo * numberThree #fill in the blank to make this 40
+testTwo = numberFive % numberOne #fill in the blank to make this 0
+testThree = numberFour - numberThree #fill in the blank to make this 7
+if testOne == 40 && testTwo == 0 && testThree == 7
+    puts true
+end
