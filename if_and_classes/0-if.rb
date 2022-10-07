539e518ca03fb9b12424d9e60daf6d9421b2ddcf
paosua86 <paolasuarezb1986@gmail.com>
2022-10-07 16:57:34 +0000

    tasks

--- if_and_classes/0-if.rb
+++ if_and_classes/0-if.rb
@@ -10,3 +10,42 @@ if a == 100
 else
   puts "#{a} is not equal to 100"
 end
+
+print "Write a number: "
+number_one = gets.chomp.to_i
+print "Write a number: "
+number_two = gets.chomp.to_i
+
+#one way
+if number_one > number_two
+  puts "Number one: #{number_one} is grater than number two #{number_two}"
+elsif number_one == number_two
+  puts "Number one: #{number_one} is equal to number two #{number_two}"
+else
+  puts "Number one: #{number_one} is less than number two #{number_two}"
+end
+
+#another way
+puts "Number one: #{number_one} is grater than number two #{number_two}" if number_one > number_two
+
+print "Type your age: "
+age = gets.chomp.to_i
+
+unless age >= 18
+  puts "You are not over 18, you can not pass"
+end
+
+
+print ("Enter user name: ")
+user = gets.chomp
+if user == "Paola"
+  puts "admin"
+else
+  puts "Visitor"
+end
+
+puts (if user == "Paola" then "admin" else "visitor" end)
+
+#if_condition_verdader ? the_result : false
+puts user == "Paola" ? "admin" : "visitor"
+
