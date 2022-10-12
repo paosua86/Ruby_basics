a26bcc8eaf218fad83e3871afe59c29fa3b50019
paosua86 <paolasuarezb1986@gmail.com>
2022-10-12 21:28:50 +0000

    task

--- matrix/0-array.rb
+++ matrix/0-array.rb
@@ -5,13 +5,26 @@
 require 'matrix'
 
 matrix1 = Matrix[[1,2,5],[1,3,5]]
+puts matrix1
 
 matrix1.each do |i|
   puts i
 end
 
-matrix2 = Matrix[[1,2,5],[1,3,5]]
-
+matrix2 = Matrix[[1,2,5],[1,3,5],[1,3,5]]
+puts
 matrix2.each(:diagonal) do |i|
   puts i
 end
+puts
+matrix2.each(:strict_lower) do |i|
+  puts i
+end
+puts
+matrix2.each(:strict_upper) do |i|
+  puts i
+end
+
+puts matrix2.diagonal?
+matrix3 = Matrix[[1,0,0],[0,3,0],[0,0,5]]
+puts matrix3.diagonal?
